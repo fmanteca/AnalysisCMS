f15b817c9a5fe1b32da57ca35bb66ae51a69af60
Pedro Fernandez <fmanteca@ifca.unican.es>
2017-04-06 12:56:28 +0200

    170406

--- test/merge.sh
+++ test/merge.sh
@@ -13,7 +13,7 @@ FOLDER="$1"
 pushd $FOLDER
 
 
-#hadd -f -k 00_Fakes.root     fakeW*.root
+hadd -f -k 00_Fakes.root     fakeW*.root
 hadd -f -k 01_Data.root      Dou*.root Sin*.root Muon*.root
 hadd -f -k 02_WZTo3LNu.root  WZTo3LNu.root
 hadd -f -k 03_VZ.root        ZZTo2L2Q*.root WZTo2L2Q__part*.root
@@ -21,7 +21,7 @@ hadd -f -k 04_Top.root       TTTo2L2*.root ST_tW_antitop.root ST_tW_top.root
 #hadd -f -k 05_ST.root        ST_tW_antitop.root ST_tW_top.root
 hadd -f -k 06_WW.root        WWTo2L2Nu.root GluGluWW*.root
 hadd -f -k 07_ZJets.root     DYJetsToLL_M-10to50.root DYJetsToTT*.root #DYJetsToLL_M-50*.root 
-hadd -f -k 08_WJets.root     WJetsToLNu.root
+#hadd -f -k 08_WJets.root     WJetsToLNu.root
 hadd -f -k 09_TTV.root       TTWJetsToLNu*.root TTWJetsToQQ.root TTZjets.root TTZToQQ.root TTZToLLNuNu_M-10.root 
 hadd -f -k 10_HWW.root       GluGluHToWWTo2L2NuAMCNLO_M125.root VBFHToWWTo2L2Nu*.root GluGluHToTauTau_M125.root VBFHToTauTau_M125.root
 hadd -f -k 11_Vg.root        Wg_MADGRAPHMLM.root Zg*.root
